c52b7d22d0b0202b21e852227dbebe361be07de1
BEROMEIRO <gabriel.romeiro2@gmail.com>
2024-03-22 17:50:12 -0300

    458

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from contact import models
 
-# Register your models here.
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'phone',)
+    ordering = ('-id',)
+    list_filter = ('created_date',)
+    search_fields = ('id', 'first_name', 'last_name',)
+    list_per_page = 50
+    list_max_show_all = 100
+    list_editable = ('last_name', 'phone',)
+    list_display_links = ('first_name',)
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = ('-id',)
